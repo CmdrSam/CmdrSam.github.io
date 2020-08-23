cfb2390449c09eff06273deb106d392d0a07c99e
Saumya Verma <saumyadsta@gmail.com>
2020-08-23 17:41:33 +0530

    made display canvas function

--- sortingApp/sort.js
+++ sortingApp/sort.js
@@ -7,26 +7,36 @@ disp = ()=>{
 
 genRandom = (num)=>{
     for(let i=0;i<num;i++){
-        rNums[i] = Math.floor(Math.random()*9)
+        rNums[i] = Math.floor(Math.random()*10)
     }
     document.getElementById("randNums").innerHTML = "[" + rNums + "]";
-    //makeLines();
+    
     bubbleSort();
 }
 
-makeLines = ()=>{
-
+let displayCanv = (arr)=>{
     let canv = document.getElementById("test");
-    var context = canv.getContext("2d");
-    for(let i=0;i<rNums.length;i++){
-        context.fillRect(10,10+20*i,rNums[i]*30,15);
-    }
-    bubbleSort();
+            var context = canv.getContext("2d");
+            context.clearRect(0, 0, canv.width, canv.height);
+
+            for(let k=0;k<arr.length;k++){
+                
+                context.fillStyle = "#86e03d";
+                context.fillRect(5+5*k,5,3,arr[k]*10);
+            }
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+// Sorting algorithms
 
 async function bubbleSort(){
     let arr = rNums;
     let n = arr.length;
+    
     for (let i = 0; i < n-1; i++){
         for (let j = 0; j < n-i-1; j++){ 
             if (arr[j] > arr[j+1]) 
@@ -37,18 +47,7 @@ async function bubbleSort(){
                 arr[j+1] = temp; 
             }
             
-            
-            // Displaying array in canvas
-            let canv = document.getElementById("test");
-            var context = canv.getContext("2d");
-            context.clearRect(0, 0, canv.width, canv.height);
-
-            for(let k=0;k<arr.length;k++){
-                
-                context.fillStyle = "#86e03d";
-                //context.fillRect(5,5+5*k,arr[k]*20,3);
-                context.fillRect(5+5*k,5,3,arr[k]*10);
-            }
+            displayCanv(arr);
             
         }
         
@@ -58,6 +57,4 @@ async function bubbleSort(){
     document.getElementById("sortedNums").innerHTML = "[" + arr + "]"; 
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+
