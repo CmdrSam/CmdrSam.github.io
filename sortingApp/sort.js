e3e4995a9ffafc06c6e1fa4f3a16c78976c3f29c
Saumya Verma <saumyadsta@gmail.com>
2020-08-23 19:05:16 +0530

    MORE CSS

--- sortingApp/sort.js
+++ sortingApp/sort.js
@@ -1,16 +1,23 @@
 var rNums = [];
 disp = ()=>{
     var n = document.getElementById("numberOfElements").value;
-    document.getElementById("show").innerHTML = "Generating array of "+n+" random numbers";
-    genRandom(n);
-    document.getElementById("sortBtn").disabled = true;
+    //document.getElementById("show").innerHTML = ;
+    if(n<50){
+        genRandom(n);
+        document.getElementById("sortBtn").disabled = true;
+    }
+    else{
+        alert("It will take a lot of time! Please enter something below 50");
+        document.getElementById("sortBtn").disabled = false;
+    }
+    
 }
 
 genRandom = (num)=>{
     for(let i=0;i<num;i++){
         rNums[i] = Math.floor(Math.random()*10)
     }
-    document.getElementById("randNums").innerHTML = "[" + rNums + "]";
+    document.getElementById("randNums").innerHTML = "<a class='lead'>Generating array of "+num+" random numbers = </a>"+ "[" + rNums + "]";
     
     bubbleSort();
 }
@@ -62,7 +69,7 @@ async function bubbleSort(){
     var endTime = performance.now();
 
     // Displaying final output and other sorts
-    document.getElementById("sortedNums").innerHTML = "Final sorted array = [" + arr + "] <br> This took " +((endTime-startTime)/1000).toFixed(2)+ " seconds";
+    document.getElementById("sortedNums").innerHTML = "<a class='lead'>Final sorted array = </a>[" + arr + "] <br><a class='lead'> This took </a>" +((endTime-startTime)/1000).toFixed(2)+ " seconds";
     document.getElementById("sortBtn").disabled = false;
     var div = document.getElementById("sortBtns");
     if(div.style.display==="none"){ div.style.display = "block"}
@@ -70,13 +77,27 @@ async function bubbleSort(){
 
 
 // Merge sort
-
+async function mergeSort(){
+    alert("I am working on that :P");
+}
 
 // Heap sort
+async function heapSort(){
+    alert("I am working on that :P");
+}
 
 
 
 //Quick sort
+async function quickSort(){
+    alert("I am working on that :P");
+}
+
+//Selection sort
+async function selectionSort(){
+    alert("I am working on that :P");
+}
+
 
 
 
